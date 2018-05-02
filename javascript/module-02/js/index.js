a336e2a580b60c4721cecd433234a928313a6ed6
Michael Matiushyn <mishamatushin@gmail.com>
2018-05-02 20:56:06 +0300

    added homework module-02, first task

---
+++ javascript/module-02/js/index.js
@@ -0,0 +1,20 @@
+let userInput;
+const numbers = [];
+let total = 0;
+
+while (userInput !== null) {
+  userInput = prompt('Введите число!');
+  const asNumber = Number(userInput);
+
+  if (asNumber) {
+    numbers.push(Number(userInput));
+  } else {
+    alert('Было введено не число, попробуйте еще раз!');
+  }
+}
+
+for (let i = 0, max = numbers.length; i < max; i += 1) {
+  total += numbers[i];
+}
+
+alert(`Общая сумма чисел равна ${total}`);
